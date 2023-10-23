d3def7234019fb24b48e6707f791eae72b32a634
Антон Ванилар <koryakinanton3@gmail.com>
2023-10-24 00:25:00 +0300

    add constants for ProfileHeaderView

--- Navigation/Controllers/ProfileViewController.swift
+++ Navigation/Controllers/ProfileViewController.swift
@@ -3,22 +3,58 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-
+    let ProfileHeaderView1: UIView = {
+        let ProfileHeaderView = UIView()
+        ProfileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        return ProfileHeaderView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Профиль"
         view.backgroundColor = .lightGray
-        let profileView = ProfileHeaderView()
-        self.view.addSubview(profileView)
+        view.addSubview(ProfileHeaderView1)
+        constraintsProfileHeaderView()
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
 
-        let profileView = ProfileHeaderView(frame: self.view.bounds)
+        let profileView = Navigation.ProfileHeaderView(frame: self.view.bounds)
         self.view.addSubview(profileView)
       }
+    
+    func constraintsProfileHeaderView() {
+        NSLayoutConstraint(item: ProfileHeaderView1,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: ProfileHeaderView1,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .trailingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: ProfileHeaderView1,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .topMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: ProfileHeaderView1,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 200).isActive = true
+    }
 }
 
 
