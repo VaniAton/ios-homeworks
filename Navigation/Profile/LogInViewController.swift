0b6890f3f94c89aab38c89ecd18faf8b3e470909
Антон Ванилар <koryakinanton3@gmail.com>
2023-12-11 13:47:23 +0300

    add elements in LoginVC and create layout

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    // Изображение логотипа
     private lazy var imageLanchScreen: UIImageView = {
         let imageLanchScreen = UIImageView()
         imageLanchScreen.translatesAutoresizingMaskIntoConstraints = false
@@ -10,85 +11,130 @@ class LogInViewController: UIViewController {
        return imageLanchScreen
     }()
     
-    private lazy var loginTable: UITableView = {
-        let loginTable = UITableView.init(
-            frame: .zero,
-            style: .plain)
-        loginTable.translatesAutoresizingMaskIntoConstraints = false
-        
-        return loginTable
-    }()
-    
+    // поле логина
     private lazy var emailButton: UITextField = {
         let emailButton =  UITextField()
-        emailButton.translatesAutoresizingMaskIntoConstraints = false
         emailButton.placeholder = "Email of phone"
-        emailButton.layer.borderColor = UIColor.lightGray.cgColor
         emailButton.layer.borderWidth = 0.5
-        emailButton.layer.cornerRadius = 10
-        emailButton.backgroundColor = .systemGray
+        emailButton.backgroundColor = .systemGray6
+        emailButton.textColor = .black
+        emailButton.autocapitalizationType = .none
+        emailButton.font = .systemFont(ofSize: 16)
         
         return emailButton
     }()
     
+    // поле пароля
     private lazy var passwordButton: UITextField = {
         let passwordButton =  UITextField()
-        passwordButton.translatesAutoresizingMaskIntoConstraints = false
         passwordButton.placeholder = "Password"
         passwordButton.isSecureTextEntry = true
-        passwordButton.backgroundColor = .systemGray
-        passwordButton.layer.borderColor = UIColor.lightGray.cgColor
+        passwordButton.backgroundColor = .systemGray6
         passwordButton.layer.borderWidth = 0.5
-        passwordButton.layer.cornerRadius = 10
+        passwordButton.textColor = .black
+        passwordButton.autocapitalizationType = .none
+        passwordButton.font = UIFont.systemFont(ofSize: 16)
         
         return passwordButton
     }()
     
+    // UIStackView с логином и паролем
+    private lazy var loginStack: UIStackView = { [unowned self] in
+        let loginStack = UIStackView()
+        
+        loginStack.translatesAutoresizingMaskIntoConstraints = false
+        loginStack.clipsToBounds = true
+        
+        loginStack.axis = .vertical
+        loginStack.distribution = .fillEqually
+        loginStack.alignment = .fill
+        loginStack.layer.cornerRadius = 10
+        loginStack.layer.borderColor = UIColor.lightGray.cgColor
+        loginStack.layer.borderWidth = 0.5
+        
+        
+        loginStack.addArrangedSubview(self.emailButton)
+        loginStack.addArrangedSubview(self.passwordButton)
+        
+        return loginStack
+    }()
+    
+    // кнопка логина
     private lazy var logInButton: UIButton = {
         let logInButton = UIButton()
         logInButton.translatesAutoresizingMaskIntoConstraints = false
-        logInButton.layer.cornerRadius = 16
+        logInButton.layer.cornerRadius = 10
         logInButton.setTitle("Log in", for: .normal)
-        logInButton.backgroundColor = .systemBlue
+        logInButton.backgroundColor = UIColor(patternImage: UIImage(named: "blue_pixel")!)
         logInButton.titleLabel?.textColor = .white
-        logInButton.layer.shadowColor = UIColor.black.cgColor
-        logInButton.layer.shadowOpacity = 0.7
-        logInButton.layer.shadowRadius = 4
-        logInButton.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
         logInButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         
         return logInButton
     }()
     
+    // добавление вью
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(imageLanchScreen)
+        view.addSubview(loginStack)
         view.addSubview(logInButton)
-        constraintsForLogin()
+        setupContraints()
+        self.hideNavigationBar()
     }
-    override func viewWillAppear(_ animated: Bool) {
-          self.navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
+    // Прячем навигационный бар в VC
+    func hideNavigationBar() {
+            self.navigationController?.setNavigationBarHidden(true, animated: true)
+        }
+    func showNavigationBar() {
+            self.navigationController?.setNavigationBarHidden(false, animated: true)
+        }
+    
+    // механизм работы кнопки
     @objc func buttonPressed() {
         let profileViewCOntroller = ProfileViewController()
         profileViewCOntroller.modalPresentationStyle = .fullScreen
     
         navigationController?.pushViewController(profileViewCOntroller, animated: true)
     }
-    func constraintsForLogin() {
-        let safeArea = view.safeAreaLayoutGuide
+    
+    // констрейнты для вью
+    func setupContraints() {
+        
+        let safeAreaGuide = view.safeAreaLayoutGuide
+        
         NSLayoutConstraint.activate([
-            imageLanchScreen.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 120),
-            imageLanchScreen.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            imageLanchScreen.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 120),
+            imageLanchScreen.centerXAnchor.constraint(equalTo: safeAreaGuide.centerXAnchor),
             imageLanchScreen.widthAnchor.constraint(equalToConstant: 100),
             imageLanchScreen.heightAnchor.constraint(equalToConstant: 100),
             
-            logInButton.topAnchor.constraint(equalTo: passwordButton.bottomAnchor, constant: 16),
-            logInButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
-            logInButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            loginStack.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16),
+            loginStack.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16),
+            loginStack.topAnchor.constraint(equalTo: imageLanchScreen.bottomAnchor, constant: 120),
+            loginStack.heightAnchor.constraint(equalToConstant: 100),
+            
+            logInButton.topAnchor.constraint(equalTo: loginStack.bottomAnchor, constant: 16),
+            logInButton.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16),
+            logInButton.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16),
             logInButton.heightAnchor.constraint(equalToConstant: 50),
         
         ])
     }
 }
+
+// структура для таблицы
+struct LabelLogin {
+    let name: String
+    
+}
+    
+    extension LabelLogin {
+        
+        static func make() -> [LabelLogin] {
+            [
+                LabelLogin(name: "Login or Number"),
+                LabelLogin(name: "Password"),
+            ]
+        }
+    }
