8d1fa4cdebc1d3a35cdcd31876a7b34579a47762
Антон Ванилар <koryakinanton3@gmail.com>
2023-12-25 23:14:18 +0300

    create struct Post and clean code in ProfileVC

--- Navigation/Post.swift
+++ Navigation/Post.swift
@@ -8,3 +8,40 @@ struct Post {
     var likes: Int
     var views: Int
 }
+    
+    extension Post {
+        
+        static func make() -> [Post] {
+            [
+                Post(
+                    author: <#T##String#>,
+                    description: <#T##String#>,
+                    image: <#T##String#>,
+                    likes: <#T##Int#>,
+                    views: 789
+                ),
+                Post(
+                    author: <#T##String#>,
+                    description: <#T##String#>,
+                    image: <#T##String#>,
+                    likes: <#T##Int#>,
+                    views: 953
+                ),
+                Post(
+                    author: <#T##String#>,
+                    description: <#T##String#>,
+                    image: <#T##String#>,
+                    likes: <#T##Int#>,
+                    views: 261
+                ),
+                Post(
+                    author: <#T##String#>,
+                    description: <#T##String#>,
+                    image: <#T##String#>,
+                    likes: <#T##Int#>,
+                    views:  1237
+                ),
+            ]
+        }
+    }
+
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -1,46 +1,19 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
-    let profileView = ProfileHeaderView()
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    lazy var changeTitile: UIButton = {
-        let changeTitile = UIButton()
-        changeTitile.translatesAutoresizingMaskIntoConstraints = false
-        changeTitile.backgroundColor = .blue
-        
-        return changeTitile
-    }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        title = "Профиль"
-        view.backgroundColor = .lightGray
-        self.view.addSubview(profileView)
-        self.view.addSubview(changeTitile)
-        setupContraintsForProfileHV()
-    }
-
-    override func viewWillLayoutSubviews() {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
     
-    func setupContraintsForProfileHV() {
-        let safeAreaGuide = view.safeAreaLayoutGuide
-        profileView.backgroundColor = .white
-        profileView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            profileView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: 0),
-            profileView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 0),
-            profileView.heightAnchor.constraint(equalToConstant: 220),
-            profileView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 0),
-            
-            changeTitile.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: 0),
-            changeTitile.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 0),
-            changeTitile.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor, constant: 0)
-        ])
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
+    
 }
 
 
