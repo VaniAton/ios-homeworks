f56698813251f26d4b1d45275ccbbace038c5f12
Антон Ванилар <koryakinanton3@gmail.com>
2024-01-22 23:18:57 +0300

    create massive in new file PostMass

--- Navigation/Post.swift
+++ Navigation/Post.swift
@@ -1,5 +1,4 @@
 import Foundation
-import UIKit
 
 struct Post {
     let author: String
@@ -8,40 +7,3 @@ struct Post {
     var likes: Int
     var views: Int
 }
-    
-    extension Post {
-        
-        static func make() -> [Post] {
-            [
-                Post(
-                    author: "FGTeeV",
-                    description: "My first post in this website!",
-                    image: "post1User1",
-                    likes: 73,
-                    views: 789
-                ),
-                Post(
-                    author: "JuegaGerman",
-                    description: "My first post in this website!",
-                    image: "post2User2",
-                    likes: 54,
-                    views: 953
-                ),
-                Post(
-                    author: "elrubiusOMG",
-                    description: "My first post in this website!",
-                    image: "post3User3",
-                    likes: 112,
-                    views: 261
-                ),
-                Post(
-                    author: "Pewdiepie",
-                    description: "My first post in this website!",
-                    image: "post4User4",
-                    likes: 305,
-                    views:  1237
-                ),
-            ]
-        }
-    }
-
---
+++ Navigation/PostMass.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+let postMass: [Post] = [
+        Post(
+            author: "FGTeeV",
+            description: "My first post in this website!",
+            image: "post1User1",
+            likes: 73,
+            views: 789
+        ),
+        Post(
+            author: "JuegaGerman",
+            description: "My first post in this website!",
+            image: "post2User2",
+            likes: 54,
+            views: 953
+        ),
+        Post(
+            author: "elrubiusOMG",
+            description: "My first post in this website!",
+            image: "post3User3",
+            likes: 112,
+            views: 261
+        ),
+        Post(
+            author: "Pewdiepie",
+            description: "My first post in this website!",
+            image: "post4User4",
+            likes: 305,
+            views:  1237
+        ),
+    ]
