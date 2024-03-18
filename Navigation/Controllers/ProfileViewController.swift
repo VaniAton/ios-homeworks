1e587ee6d7fff3156ad5a2180837dcf4965c5876
VanilarAnton <koryakinanton3@gmail.com>
2024-03-18 23:41:06 +0300

    create debug and release scheme and create func of change background color for ProfileVC

--- Navigation/Controllers/ProfileViewController.swift
+++ Navigation/Controllers/ProfileViewController.swift
@@ -27,10 +27,16 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+      //  view.backgroundColor = .white
         view.addSubview(tableView)
         setupConstraints()
         tuneTableView()
+        
+        #if DEBUG
+        view.backgroundColor = .red
+        #else
+        view.backgroundColor = .systemGray
+        #endif
         
     }
     
